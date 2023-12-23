2e461a033f558c2f68e6da2d8004cce974a6713a
casualzach <76960863+casualzach@users.noreply.github.com>
2023-12-23 14:49:06 +0100

    tailwind prettier addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  let number = 0;
+  const number = 0;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>{number}</div>
